86f76b4ca8cb8aa26f3d1e71bd587fee234867f6
David <david.melikyan.1999@mail.ru>
2020-07-20 00:37:10 +0400

    Added binding adapters for first fragment

--- app/src/main/java/com/example/dynamicmessenger/authorization/fragments/EmailAndPhoneFragment.kt
+++ app/src/main/java/com/example/dynamicmessenger/authorization/fragments/EmailAndPhoneFragment.kt
@@ -1,80 +1,38 @@
 package com.example.dynamicmessenger.authorization.fragments
 
-import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.dynamicmessenger.R
 import com.example.dynamicmessenger.authorization.viewModels.EmailAndPhoneViewModel
 import com.example.dynamicmessenger.databinding.FragmentEmailAndPhoneBinding
-import com.example.dynamicmessenger.network.authorization.models.EmailExistTask
 import com.example.dynamicmessenger.utils.Validations
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 
 
 class EmailAndPhoneFragment : Fragment() {
     private lateinit var viewModel: EmailAndPhoneViewModel
+    private lateinit var binding: FragmentEmailAndPhoneBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 //        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()TODO
-        val binding: FragmentEmailAndPhoneBinding =
-            FragmentEmailAndPhoneBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_email_and_phone, container, false)
         viewModel = ViewModelProvider(this).get(EmailAndPhoneViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
-        //TODO: Binding adapters
-        binding.loginRegisterEmailEditText.addTextChangedListener(object : TextWatcher {
-            @SuppressLint("ResourceAsColor")
-            override fun afterTextChanged(s: Editable?) {
-                if (Validations.isEmailValid(binding.loginRegisterEmailEditText.text.toString())) {
-                    binding.continueButton.isEnabled = true
-                    binding.emailHintTextView.setTextColor(Color.BLUE)
-                    binding.loginRegisterEmailEditText.setBackgroundResource(R.drawable.edittext_blue_underline)
-                    binding.continueButton.setBackgroundResource(R.drawable.enable_button_design)
-                } else {
-                    binding.continueButton.isEnabled = false
-                    binding.emailHintTextView.setTextColor(Color.RED)
-                    binding.loginRegisterEmailEditText.setBackgroundResource(R.drawable.edittext_red_underline)
-                    binding.continueButton.setBackgroundResource(R.drawable.disable_button_design)
-                }
-                if (binding.loginRegisterEmailEditText.text.toString().isNotEmpty()) {
-                    binding.emailHintTextView.visibility = View.VISIBLE
-                } else {
-                    binding.emailHintTextView.visibility = View.INVISIBLE
-                }
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        viewModel.userEnteredEmail.observe(viewLifecycleOwner, Observer {
+            viewModel.hintVisibility.value = it.isNotEmpty()
+            viewModel.isEmailValid.value = Validations.isEmailValid(it)
         })
 
-        binding.continueButton.setOnClickListener {
-            binding.progressBar.visibility = View.VISIBLE
-            if (Validations.isEmailValid(binding.loginRegisterEmailEditText.text.toString())) {
-                binding.progressBar.visibility = View.VISIBLE
-                val emailExist = EmailExistTask(binding.loginRegisterEmailEditText.text.toString())
-                viewModel.emailNetwork(it, emailExist, context, binding)
-            } else {
-                binding.progressBar.visibility = View.INVISIBLE
-                Toast.makeText(context, "Invalid email", Toast.LENGTH_SHORT).show()
-            }
-        }
         return binding.root
     }
 }
-
--- app/src/main/java/com/example/dynamicmessenger/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/dynamicmessenger/utils/BindingAdapters.kt
@@ -1,14 +1,59 @@
 package com.example.dynamicmessenger.utils
 
-import android.widget.ImageView
+import android.graphics.Color
+import android.view.View
+import android.widget.*
 import androidx.databinding.BindingAdapter
 import com.example.dynamicmessenger.R
-import com.example.dynamicmessenger.common.AppLangKeys
-import com.example.dynamicmessenger.common.SharedConfigs
 
-//class BindingAdapters {
-    @BindingAdapter("languageIMG")
-    fun setLanguageIG(image: ImageView, resource: Int) {
-        image.setImageResource(resource)
+@BindingAdapter("languageIMG")
+fun setLanguageIG(image: ImageView, resource: Int) {
+    image.setImageResource(resource)
+}
+
+@BindingAdapter("setTextViewVisible")
+fun setTextViewVisible(textView: TextView, answer: Boolean) {
+    if (answer) {
+        textView.visibility = View.VISIBLE
+    } else {
+        textView.visibility = View.INVISIBLE
     }
-//}
+}
+
+@BindingAdapter("setHintColour")
+fun setHintColour(textView: TextView, answer: Boolean) {
+    if (answer) {
+        textView.setTextColor(Color.BLUE)
+    } else {
+        textView.setTextColor(Color.RED)
+    }
+}
+
+@BindingAdapter("setContinueButtonConfiguration")
+fun setContinueButtonConfiguration(button: Button, answer: Boolean) {
+    button.isEnabled = answer
+    if (answer) {
+        button.setBackgroundResource(R.drawable.enable_button_design)
+    } else {
+        button.setBackgroundResource(R.drawable.disable_button_design)
+    }
+}
+
+@BindingAdapter("setEditTextBackground")
+fun setEditTextBackground(editText: EditText, answer: Boolean) {
+    if (answer) {
+        editText.setBackgroundResource(R.drawable.edittext_blue_underline)
+    } else {
+        editText.setBackgroundResource(R.drawable.edittext_red_underline)
+    }
+}
+
+@BindingAdapter("setProgressBarVisible")
+fun setProgressBarVisible(progressBar: ProgressBar, answer: Boolean) {
+    if (answer) {
+        progressBar.visibility = View.VISIBLE
+    } else {
+        progressBar.visibility = View.INVISIBLE
+    }
+}
+
